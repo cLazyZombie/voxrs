cb56aba6eb1c105d15bb281a86c03c5f538e2587
cLazyZombie <rusad96@gmail.com>
2021-01-14 21:58:34 +0900

    add reference counting to asset

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## todo
 - asset
- - remove unused asset
+ - reference count asset 
+ - delete unused asset ( ref cont == 0 )
+ - save asset with &dyn Asset in AssetManager
+ - test AssetManager::get("path".into())
  - async loading
-
-- log
+ - sperated not yet built asset (for performance reason)
--- src/asset/assets.rs
+++ src/asset/assets.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, marker::PhantomData};
+use std::{collections::HashMap, marker::PhantomData, sync::Arc};
 use std::hash::Hash;
 
 use wgpu::util::make_spirv;
@@ -46,6 +46,7 @@ pub struct AssetId(u64);
 
 pub struct AssetHandle<T: Asset> {
     hash: AssetHash,
+    rc: Arc<()>,
     _marker: PhantomData<T>,
 }
 
@@ -53,15 +54,17 @@ impl<T: Asset> Clone for AssetHandle<T> {
     fn clone(&self) -> Self {
         Self {
             hash: self.hash,
+            rc: Arc::clone(&self.rc),
             _marker: PhantomData,
         }
     }
 }
 
 impl<T: Asset> AssetHandle<T> {
-    fn new(hash: AssetHash) -> Self {
+    fn new(hash: AssetHash, rc: Arc<()>) -> Self {
         Self {
             hash,
+            rc,
             _marker: PhantomData,
         }
     }
@@ -165,10 +168,25 @@ impl ShaderAsset {
     }
 }
 
+
+struct RcAsset<T: Asset> {
+    asset: T,
+    rc: Arc<()>,
+}
+
+impl<T: Asset> RcAsset<T> {
+    fn new(asset: T) -> Self {
+        Self {
+            asset,
+            rc: Arc::new(()),
+        }
+    }
+}
+
 pub struct AssetManager<F: FileSystem> {
-    textures: HashMap<AssetHash, TextureAsset>,
-    texts: HashMap<AssetHash, TextAsset>,
-    shaders: HashMap<AssetHash, ShaderAsset>,
+    textures: HashMap<AssetHash, RcAsset<TextureAsset>>,
+    texts: HashMap<AssetHash, RcAsset<TextAsset>>,
+    shaders: HashMap<AssetHash, RcAsset<ShaderAsset>>,
     _marker: std::marker::PhantomData<F>,
 }
 
@@ -192,14 +210,19 @@ impl<F: FileSystem> AssetManager<F> {
 
     fn get_text<T: Asset>(&mut self, path: &AssetPath) -> Option<AssetHandle<T>> {
         let hash = path.get_hash();
-        if let Some(_text) = self.texts.get(&hash) {
-            Some(AssetHandle::new(hash))
+        if let Some(rc_asset) = self.texts.get(&hash) {
+            let rc = Arc::clone(&rc_asset.rc);
+            Some(AssetHandle::new(hash, rc))
         } else {
             // load from io
             if let Ok(read) = F::read_text(&path.path) {
                 let text_asset = TextAsset::new(read);
-                self.texts.insert(hash, text_asset);
-                Some(AssetHandle::new(hash))
+                let rc_asset = RcAsset::new(text_asset);
+                let cloned = Arc::clone(&rc_asset.rc);
+
+                self.texts.insert(hash, rc_asset);
+                
+                Some(AssetHandle::new(hash, cloned))
             } else {
                 None
             }
@@ -208,12 +231,16 @@ impl<F: FileSystem> AssetManager<F> {
 
     fn get_texture<T: Asset>(&mut self, path: &AssetPath) -> Option<AssetHandle<T>> {
         let hash = path.get_hash();
-        if let Some(_texture) = self.textures.get(&hash) {
-            Some(AssetHandle::new(hash))
+        if let Some(rc_asset) = self.textures.get(&hash) {
+            let rc = Arc::clone(&rc_asset.rc);
+            Some(AssetHandle::new(hash, rc))
         } else if let Ok(read) = F::read_binary(&path.path) {
             let texture_asset = TextureAsset::new(read);
-            self.textures.insert(hash, texture_asset);
-            Some(AssetHandle::new(hash))
+            let rc_asset = RcAsset::new(texture_asset);
+            let clonned = Arc::clone(&rc_asset.rc);
+            self.textures.insert(hash, rc_asset);
+
+            Some(AssetHandle::new(hash, clonned))
         } else {
             None
         }
@@ -221,12 +248,16 @@ impl<F: FileSystem> AssetManager<F> {
 
     fn get_shader<T: Asset>(&mut self, path: &AssetPath) -> Option<AssetHandle<T>> {
         let hash = path.get_hash();
-        if let Some(_shader) = self.shaders.get(&hash) {
-            Some(AssetHandle::new(hash))
+        if let Some(rc_asset) = self.shaders.get(&hash) {
+            let rc = Arc::clone(&rc_asset.rc);
+            Some(AssetHandle::new(hash, rc))
         } else if let Ok(read) = F::read_binary(&path.path) {
             let shader_asset = ShaderAsset::new(read);
-            self.shaders.insert(hash, shader_asset);
-            Some(AssetHandle::new(hash))
+            let rc_asset = RcAsset::new(shader_asset);
+            let clonned = Arc::clone(&rc_asset.rc);
+            self.shaders.insert(hash, rc_asset);
+
+            Some(AssetHandle::new(hash, clonned))
         } else {
             None
         }
@@ -237,39 +268,67 @@ impl<F: FileSystem> AssetManager<F> {
             AssetType::Text => {
                 let text = self.texts.get(&handle.hash).unwrap();
                 unsafe {
-                    let p : *const T = (text as *const TextAsset).cast();
+                    let p : *const T = (&text.asset as *const TextAsset).cast();
                     &*p
                 }
             }
             AssetType::Texture => {
                 let texture = self.textures.get(&handle.hash).unwrap();
                 unsafe {
-                    let p : *const T = (texture as *const TextureAsset).cast();
+                    let p : *const T = (&texture.asset as *const TextureAsset).cast();
                     &*p
                 }
             }
             AssetType::Shader => {
                 let shader = self.shaders.get(&handle.hash).unwrap();
                 unsafe {
-                    let p : *const T = (shader as *const ShaderAsset).cast();
+                    let p : *const T = (&shader.asset as *const ShaderAsset).cast();
                     &*p
                 }
             }
         }
     }
 
+    #[cfg(test)]
+    fn get_rc<T: Asset>(&self, path: &AssetPath) -> Option<usize> {
+        let hash = path.get_hash();
+        match T::ASSET_TYPE {
+            AssetType::Text => {
+                if let Some(rc_asset) = self.texts.get(&hash) {
+                    Some(Arc::strong_count(&rc_asset.rc) - 1)
+                } else {
+                    None
+                }
+            }
+            AssetType::Texture => {
+                if let Some(rc_asset) = self.textures.get(&hash) {
+                    Some(Arc::strong_count(&rc_asset.rc) - 1)
+                } else {
+                    None
+                }
+            }
+            AssetType::Shader => {
+                if let Some(rc_asset) = self.shaders.get(&hash) {
+                    Some(Arc::strong_count(&rc_asset.rc) - 1)
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
     pub fn build_textures(&mut self, device: &wgpu::Device, queue: &wgpu::Queue) {
-        for texture in self.textures.values_mut() {
-            if !texture.texture.is_built() {
-                texture.build(device, queue);
+        for RcAsset{asset, ..} in self.textures.values_mut() {
+            if !asset.texture.is_built() {
+                asset.build(device, queue);
             }
         }
     }
 
     pub fn build_shaders(&mut self, device: &wgpu::Device) {
-        for shader in self.shaders.values_mut() {
-            if !shader.module.is_built() {
-                shader.build(device);
+        for RcAsset{ asset, ..} in self.shaders.values_mut() {
+            if !asset.module.is_built() {
+                asset.build(device);
             }
         }
     }
@@ -301,5 +360,20 @@ mod tests {
 
         let texture_asset = manager.get_asset(&handle.unwrap());
         assert_eq!(texture_asset.buf, include_bytes!("../test_assets/texture.png"));
+    }
+
+    #[test]
+    fn get_rc_test() {
+        let mut manager = AssetManager::<MockFileSystem>::new();
+        let path : AssetPath = "test.txt".into();
+        assert!(manager.get_rc::<TextAsset>(&path).is_none());
+
+        let handle = manager.get::<TextAsset>(&path).unwrap();
+
+        assert_eq!(manager.get_rc::<TextAsset>(&path).unwrap(), 1);
+
+        drop(handle);
+
+        assert_eq!(manager.get_rc::<TextAsset>(&path).unwrap(), 0);
     }
 }
