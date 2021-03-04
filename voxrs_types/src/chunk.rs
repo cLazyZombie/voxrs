ad76f65f8e42ba06260ae35e860d95a80861492d
cLazyZombie <rusad96@gmail.com>
2021-03-04 11:42:50 +0900

    implements voxrs_types

--- voxrs_types/src/chunk.rs
+++ voxrs_types/src/chunk.rs
@@ -29,12 +29,24 @@ pub struct WorldBlockCounts {
     z: i32,
 }
 
+impl WorldBlockCounts {
+    pub fn new(x: i32, y: i32, z: i32) -> Self {
+        Self { x, y, z }
+    }
+}
+
 /// chunk count in world in each direction (x, y, z)
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct WorldChunkCounts {
     x: i32,
     y: i32,
     z: i32,
+}
+
+impl WorldChunkCounts {
+    pub fn new(x: i32, y: i32, z: i32) -> Self {
+        Self { x, y, z }
+    }
 }
 
 impl From<WorldBlockCounts> for WorldChunkCounts {
@@ -49,20 +61,20 @@ impl From<WorldBlockCounts> for WorldChunkCounts {
 
 #[derive(Copy, Clone, Debug)]
 pub struct ChunkPos {
-    block_counts: WorldBlockCounts,
+    chunk_counts: WorldChunkCounts,
     chunk_idx: i32,
 }
 
 impl ChunkPos {
-    pub fn new(block_counts: WorldBlockCounts, idx: i32) -> Self {
+    pub fn new(chunk_counts: WorldChunkCounts, idx: i32) -> Self {
         Self {
-            block_counts,
+            chunk_counts,
             chunk_idx: idx,
         }
     }
 
     pub fn neighbor_chunk_pos(&self, dir: Dir) -> Option<Self> {
-        let xyz : (i32, i32, i32) = self.into();
+        let xyz: (i32, i32, i32) = self.get_xyz();
         let nxyz = match dir {
             Dir::XPos => (xyz.0 + 1, xyz.1, xyz.2),
             Dir::XNeg => (xyz.0 - 1, xyz.1, xyz.2),
@@ -72,97 +84,109 @@ impl ChunkPos {
             Dir::ZNeg => (xyz.0, xyz.1, xyz.2 - 1),
         };
 
-        let world_chunk_count: WorldChunkCounts = self.block_counts.into();
-
-        if ChunkPos::is_xyz_in_world(world_chunk_count, nxyz) {
-            let chunk_idx = nxyz.0 + nxyz.1 * world_chunk_count.x + nxyz.2 * world_chunk_count.x * world_chunk_count.y;
-            Some(ChunkPos::new(self.block_counts, chunk_idx))
+        if ChunkPos::is_xyz_in_world(self.chunk_counts, nxyz) {
+            let chunk_idx = ChunkPos::xyz_to_idx(self.chunk_counts, nxyz);
+            Some(ChunkPos::new(self.chunk_counts, chunk_idx))
         } else {
             None
         }
     }
 
+    fn xyz_to_idx(world_chunk_count: WorldChunkCounts, xyz: (i32, i32, i32)) -> i32 {
+        let chunk_idx =
+            xyz.0 + xyz.1 * world_chunk_count.x + xyz.2 * world_chunk_count.x * world_chunk_count.y;
+        chunk_idx
+    }
+
     fn is_xyz_in_world(world_chunk_count: WorldChunkCounts, xyz: (i32, i32, i32)) -> bool {
-        xyz.0 >= 0 &&
-        xyz.0 < world_chunk_count.x &&
-        xyz.1 >= 0 &&
-        xyz.1 < world_chunk_count.y &&
-        xyz.2 >= 0 &&
-        xyz.2 < world_chunk_count.z
+        xyz.0 >= 0
+            && xyz.0 < world_chunk_count.x
+            && xyz.1 >= 0
+            && xyz.1 < world_chunk_count.y
+            && xyz.2 >= 0
+            && xyz.2 < world_chunk_count.z
+    }
+
+    /// get xyz coord in world (chunk unit)
+    fn get_xyz(&self) -> (i32, i32, i32) {
+        let x = self.chunk_idx % self.chunk_counts.x;
+        let y = self.chunk_idx / self.chunk_counts.x % self.chunk_counts.y;
+        let z = self.chunk_idx / (self.chunk_counts.x * self.chunk_counts.y);
+
+        (x, y, z)
     }
 }
 
 impl From<&BlockPos> for ChunkPos {
     fn from(block_pos: &BlockPos) -> Self {
         Self {
-            block_counts: block_pos.block_counts,
+            chunk_counts: block_pos.chunk_counts,
             chunk_idx: block_pos.chunk_idx,
         }
     }
 }
 
-impl From<&ChunkPos> for (i32, i32, i32) {
-    fn from(chunk_pos: &ChunkPos) -> Self {
-        let world_chunk_count: WorldChunkCounts = chunk_pos.block_counts.into();
-        let x = chunk_pos.chunk_idx % world_chunk_count.x;
-        let y = chunk_pos.chunk_idx / world_chunk_count.x % world_chunk_count.y;
-        let z = chunk_pos.chunk_idx / (world_chunk_count.x * world_chunk_count.y);
-
-        (x, y, z)
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct BlockPos {
-    block_counts: WorldBlockCounts,
+    chunk_counts: WorldChunkCounts,
     chunk_idx: i32,
     block_idx: i32,
 }
 
 impl BlockPos {
-    pub fn new(block_counts: WorldBlockCounts, chunk_idx: i32, block_idx: i32) -> Self {
+    pub fn new(chunk_counts: WorldChunkCounts, chunk_idx: i32, block_idx: i32) -> Self {
         Self {
-            block_counts,
+            chunk_counts,
             chunk_idx,
             block_idx,
         }
     }
 
-    pub fn neighbor_block_pos(&self, dir: Dir) -> Option<BlockPos> {
-        let pos: (i32, i32, i32) = self.into();
+    pub fn from_world_xyz(
+        chunk_counts: WorldChunkCounts,
+        world_xyz: (i32, i32, i32),
+    ) -> Option<Self> {
+        if world_xyz.0 < 0 || world_xyz.1 < 0 || world_xyz.2 < 0 {
+            return None;
+        }
 
-        // todo: refactoring
-        let neighbor = match dir {
-            Dir::XPos => {
-                let npos = (pos.0 + 1, pos.1, pos.2);
+        let chunk_xyz = (
+            world_xyz.0 / BLOCK_COUNT_IN_CHUNKSIDE as i32,
+            world_xyz.1 / BLOCK_COUNT_IN_CHUNKSIDE as i32,
+            world_xyz.2 / BLOCK_COUNT_IN_CHUNKSIDE as i32,
+        );
 
-                if BlockPos::is_in_chunk(npos) {
-                    Some(BlockPos {
-                        block_counts: self.block_counts,
-                        chunk_idx: self.chunk_idx,
-                        block_idx: BlockPos::pos_to_idx(npos),
-                    })
-                } else {
-                    let neighbor_chunk_idx = self.neighbor_chunk_idx(dir);
-                    if let Some(neighbor_chunk_idx) = neighbor_chunk_idx {
-                        let npos = (BLOCK_COUNT_IN_CHUNKSIDE as i32 -1, npos.1, npos.2);
-                        Some(BlockPos {
-                            block_counts: self.block_counts,
-                            chunk_idx: neighbor_chunk_idx,
-                            block_idx: BlockPos::pos_to_idx(npos),
-                        })
-                    } else {
-                        None
-                    }
-                }
-            }
-            _ => { None }
-        };
-
-        neighbor
+        if ChunkPos::is_xyz_in_world(chunk_counts, chunk_xyz) {
+            let local_xyz = (
+                world_xyz.0 % BLOCK_COUNT_IN_CHUNKSIDE as i32,
+                world_xyz.1 % BLOCK_COUNT_IN_CHUNKSIDE as i32,
+                world_xyz.2 % BLOCK_COUNT_IN_CHUNKSIDE as i32,
+            );
+            let block_idx = BlockPos::pos_to_idx(local_xyz);
+            let chunk_idx = ChunkPos::xyz_to_idx(chunk_counts, chunk_xyz);
+            Some(BlockPos::new(chunk_counts, chunk_idx, block_idx))
+        } else {
+            None
+        }
     }
 
-    fn neighbor_chunk_idx(&self, dir: Dir) -> Option<i32> {
+    pub fn neighbor_block_pos(&self, dir: Dir) -> Option<BlockPos> {
+        let mut world_xyz = self.get_world_xyz();
+
+        match dir {
+            Dir::XPos => world_xyz.0 += 1,
+            Dir::XNeg => world_xyz.0 -= 1,
+            Dir::YPos => world_xyz.1 += 1,
+            Dir::YNeg => world_xyz.1 -= 1,
+            Dir::ZPos => world_xyz.2 += 1,
+            Dir::ZNeg => world_xyz.2 -= 1,
+        }
+
+        let neighbor_pos = BlockPos::from_world_xyz(self.chunk_counts, world_xyz);
+        neighbor_pos
+    }
+
+    pub fn neighbor_chunk_idx(&self, dir: Dir) -> Option<i32> {
         let chunk_pos: ChunkPos = self.into();
         let neighbor_chunk = chunk_pos.neighbor_chunk_pos(dir);
         if let Some(chunk) = neighbor_chunk {
@@ -172,30 +196,78 @@ impl BlockPos {
         }
     }
 
-    fn is_in_chunk(pos: (i32, i32, i32)) -> bool {
-        if pos.0 >= 0
-            && pos.0 < BLOCK_COUNT_IN_CHUNKSIDE as i32
-            && pos.1 >= 0
-            && pos.1 < BLOCK_COUNT_IN_CHUNKSIDE as i32
-            && pos.2 >= 0
-            && pos.2 < BLOCK_COUNT_IN_CHUNKSIDE as i32
-        {
-            true
-        } else {
-            false
-        }
+    fn pos_to_idx(pos: (i32, i32, i32)) -> i32 {
+        pos.0
+            + pos.1 * BLOCK_COUNT_IN_CHUNKSIDE as i32
+            + pos.2 * (BLOCK_COUNT_IN_CHUNKSIDE * BLOCK_COUNT_IN_CHUNKSIDE) as i32
     }
 
-    fn pos_to_idx(pos: (i32, i32, i32)) -> i32 {
-        pos.0 + pos.1 * BLOCK_COUNT_IN_CHUNKSIDE as i32 + pos.2 * (BLOCK_COUNT_IN_CHUNKSIDE * BLOCK_COUNT_IN_CHUNKSIDE) as i32
+    /// get local xyz of block in chunk
+    fn get_local_xyz(&self) -> (i32, i32, i32) {
+        let x = self.block_idx % BLOCK_COUNT_IN_CHUNKSIDE as i32;
+        let y = self.block_idx / BLOCK_COUNT_IN_CHUNKSIDE as i32 % BLOCK_COUNT_IN_CHUNKSIDE as i32;
+        let z = self.block_idx / (BLOCK_COUNT_IN_CHUNKSIDE * BLOCK_COUNT_IN_CHUNKSIDE) as i32;
+        (x as i32, y as i32, z as i32)
+    }
+
+    /// get world xyz of lbock
+    fn get_world_xyz(&self) -> (i32, i32, i32) {
+        let chunk_pos: ChunkPos = self.into();
+        let chunk_xyz: (i32, i32, i32) = chunk_pos.get_xyz();
+
+        let local_xyz = self.get_local_xyz();
+
+        (
+            local_xyz.0 + chunk_xyz.0 * BLOCK_COUNT_IN_CHUNKSIDE as i32,
+            local_xyz.1 + chunk_xyz.1 * BLOCK_COUNT_IN_CHUNKSIDE as i32,
+            local_xyz.2 + chunk_xyz.2 * BLOCK_COUNT_IN_CHUNKSIDE as i32,
+        )
     }
 }
 
-impl From<&BlockPos> for (i32, i32, i32) {
-    fn from(block_pos: &BlockPos) -> Self {
-        let x = block_pos.block_idx % BLOCK_COUNT_IN_CHUNKSIDE as i32;
-        let y = block_pos.block_idx / BLOCK_COUNT_IN_CHUNKSIDE as i32 % BLOCK_COUNT_IN_CHUNKSIDE as i32;
-        let z = block_pos.block_idx / (BLOCK_COUNT_IN_CHUNKSIDE * BLOCK_COUNT_IN_CHUNKSIDE) as i32;
-        (x as i32, y as i32, z as i32)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn neighbor_block_pos_test() {
+        let chunk_counts = WorldChunkCounts::new(2, 4, 6);
+        let block = BlockPos::from_world_xyz(chunk_counts, (16, 16, 16)).unwrap();
+        assert_eq!(block, BlockPos::new(chunk_counts, 11, 0));
+
+        let block_xpos = block.neighbor_block_pos(Dir::XPos).unwrap();
+        assert_eq!(block_xpos, BlockPos::new(chunk_counts, 11, 1));
+
+        let block_xneg = block.neighbor_block_pos(Dir::XNeg).unwrap();
+        assert_eq!(block_xneg, BlockPos::new(chunk_counts, 10, 15));
+    }
+
+    #[test]
+    fn invalid_neighbor_block_pos_test() {
+        let chunk_counts = WorldChunkCounts::new(2, 2, 2);
+
+        // first block
+        let block = BlockPos::from_world_xyz(chunk_counts, (0, 0, 0)).unwrap();
+
+        let invalid = block.neighbor_block_pos(Dir::XNeg);
+        assert_eq!(invalid, None);
+
+        let invalid = block.neighbor_block_pos(Dir::YNeg);
+        assert_eq!(invalid, None);
+
+        let invalid = block.neighbor_block_pos(Dir::ZNeg);
+        assert_eq!(invalid, None);
+
+        // last block
+        let block = BlockPos::from_world_xyz(chunk_counts, (31, 31, 31)).unwrap();
+
+        let invalid = block.neighbor_block_pos(Dir::XPos);
+        assert_eq!(invalid, None);
+
+        let invalid = block.neighbor_block_pos(Dir::YPos);
+        assert_eq!(invalid, None);
+
+        let invalid = block.neighbor_block_pos(Dir::ZPos);
+        assert_eq!(invalid, None);
     }
 }
