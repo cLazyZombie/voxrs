888ad2b142959ff95d46f1ff0dab34ab2e551b4c
cLazyZombie <rusad96@gmail.com>
2021-03-10 15:20:15 +0900

    testing matrix out of index

--- voxrs_math/src/matrix4.rs
+++ voxrs_math/src/matrix4.rs
@@ -201,4 +201,17 @@ mod tests {
             .as_slice()
         );
     }
+
+    #[test]
+    #[should_panic]
+    fn test_out_of_index() {
+        let m = Matrix4::new(
+            1.0, 2.0, 3.0, 4.0,
+            5.0, 6.0, 7.0, 8.0, 
+            9.0, 10.0, 11.0, 12.0, 
+            13.0, 14.0, 15.0, 16.0, 
+        );
+
+        let _ = m[(0, 0)];
+    }
 }
