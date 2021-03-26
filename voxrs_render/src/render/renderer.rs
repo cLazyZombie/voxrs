4694b25bcbf422c89283d48b91961ee2be56b23f
cLazyZombie <rusad96@gmail.com>
2021-03-26 12:23:35 +0900

    refactoring

--- voxrs_render/src/render/renderer.rs
+++ voxrs_render/src/render/renderer.rs
@@ -77,7 +77,7 @@ impl Renderer {
 
         let chunk_renderer = ChunkRenderSystem::new(&device, asset_manager, &view_proj_buf);
         let dynamic_block_renderer =
-            DynamicBlockRenderSystem::new(&device, asset_manager, &&view_proj_buf);
+            DynamicBlockRenderSystem::new(&device, asset_manager, &view_proj_buf);
 
         Self {
             surface,
