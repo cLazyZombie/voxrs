e9c8aa6e63756709622a441473506344334f39a1
cLazyZombie <rusad96@gmail.com>
2021-03-26 12:32:10 +0900

    make material contains vertex/frag shader

--- voxrs_asset/src/material.rs
+++ voxrs_asset/src/material.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
 use voxrs_types::io::FileSystem;
 
+use crate::ShaderAsset;
+
 use super::{
     assets::{Asset, AssetType},
     AssetHandle, AssetManager, TextureAsset,
@@ -9,11 +11,15 @@ use super::{
 #[derive(Asset)]
 pub struct MaterialAsset {
     pub diffuse_tex: AssetHandle<TextureAsset>,
+    pub vertex_shader: AssetHandle<ShaderAsset>,
+    pub frag_shader: AssetHandle<ShaderAsset>,
 }
 
 #[derive(Deserialize)]
 struct MaterialAssetRaw {
     diffuse_tex: String,
+    vertex_shader: String,
+    frag_shader: String,
 }
 
 impl MaterialAsset {
@@ -21,7 +27,13 @@ impl MaterialAsset {
         let raw: MaterialAssetRaw = serde_json::from_str(s).unwrap();
 
         let diffuse_tex = asset_manager.get::<TextureAsset>(&raw.diffuse_tex.into());
+        let vertex_shader = asset_manager.get::<ShaderAsset>(&raw.vertex_shader.into());
+        let frag_shader = asset_manager.get::<ShaderAsset>(&raw.frag_shader.into());
 
-        Self { diffuse_tex }
+        Self {
+            diffuse_tex,
+            vertex_shader,
+            frag_shader,
+        }
     }
 }
