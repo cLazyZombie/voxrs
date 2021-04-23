07f5a4ce9bb2cb3485fa5adea86093ed89fc4663
cLazyZombie <rusad96@gmail.com>
2021-04-23 17:31:30 +0900

    rect2 transform

--- voxrs_math/src/rect.rs
+++ voxrs_math/src/rect.rs
@@ -22,4 +22,31 @@ impl Rect2 {
     pub fn max(&self) -> Vec2 {
         self.min + self.size
     }
+
+    /// move self start from parent.min
+    /// and clip using parent area
+    pub fn transform(&self, parent: Rect2) -> Rect2 {
+        let min = self.min + parent.min;
+        let max = min + self.size;
+
+        let min = min.max(parent.min);
+        let max = max.min(parent.max());
+
+        Rect2::from_min_max(min, max)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_transform() {
+        let parent = Rect2::new((10.0, 10.0).into(), (100.0, 100.0).into());
+        let child = Rect2::new((10.0, 20.0).into(), (100.0, 200.0).into());
+        let transformed = child.transform(parent);
+
+        assert!(transformed.min.abs_diff_eq(Vec2::new(20.0, 30.0), 0.1));
+        assert!(transformed.size.abs_diff_eq(Vec2::new(90.0, 80.0), 0.1));
+    }
 }
--- voxrs_ui/src/widget/mod.rs
+++ voxrs_ui/src/widget/mod.rs
@@ -31,14 +31,8 @@ impl Widget {
     }
 
     fn intersect_region(&self, parent_region: Rect2) -> Rect2 {
-        let my_region = self.widget_region();
-        let min = my_region.min + parent_region.min;
-        let max = min + my_region.size;
-
-        let min = min.max(parent_region.min);
-        let max = max.min(parent_region.max());
-
-        Rect2::from_min_max(min, max)
+        let region = self.widget_region();
+        region.transform(parent_region)
     }
 
     fn widget_region(&self) -> Rect2 {
